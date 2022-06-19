94ab745468ce39abbc3078a6cc12a253fb920bd5
EvgeniyKolesnikov <mr.oneofus@gmail.com>
2022-06-19 15:32:45 +0300

    Initial Commit

---
+++ VKS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKS
+//
+//  Created by Evgeniy Kolesnikov on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
