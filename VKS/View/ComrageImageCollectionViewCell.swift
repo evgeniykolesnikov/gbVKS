1d4691bc47404e019dd5418eb0bd56dcb9b3efd1
EvgeniyKolesnikov <mr.oneofus@gmail.com>
2022-07-04 16:24:19 +0300

    add folders and put files into them, add CollectionViewController

---
+++ VKS/View/ComrageImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+  //
+//  ComrageImageCollectionViewCell.swift
+//  VKS
+//
+//  Created by Evgeniy Kolesnikov on 04.07.2022.
+//
+
+import UIKit
+
+class ComrageImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var nameComrade: UILabel!
+    @IBOutlet var imageComrade: UIImageView!
+    @IBOutlet var textComrede: UILabel!
+}
