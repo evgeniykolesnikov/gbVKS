ccd5ccc7df543bceea6540e808c67685b835d4aa
EvgeniyKolesnikov <mr.oneofus@gmail.com>
2022-09-02 12:02:31 +0300

    add pods to project

---
+++ VKS/Controller/Sc/ScUrlSecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScUrlSecondViewController.swift
+//  VKS
+//
+//  Created by Evgeniy Kolesnikov on 01.09.2022.
+//
+
+import UIKit
+import Alamofire
+
+class ScUrlSecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- VKS/Controller/Sc/ScUrlViewController.swift
+++ VKS/Controller/Sc/ScUrlViewController.swift
@@ -9,31 +9,29 @@ import UIKit
 
 class ScUrlViewController: UIViewController {
 
-    //1 url
-    let url = URL(string: "http://samples.openweathermap.org/data/2.5/forecast?q=Moscow,DE&appid=b1b15e88f")
-//http://samples.openweathermap.org/data/2.5/forecast?q=Moscow,DE&appid=b1b15e88f
-
     override func viewDidLoad() {
         super.viewDidLoad()
         var urlCons = URLComponents()
-        urlCons.scheme = "http"
-        urlCons.host = "samples.openweathermap.org"
-        urlCons.path = "/data/2.5/forecast"
+        urlCons.scheme = "https"
+        urlCons.host = "jsonplaceholder.typicode.com"
+        urlCons.path = "/post"
 
         urlCons.queryItems = [
-            URLQueryItem(name: "q", value: "Moscow,DE"),
-            URLQueryItem(name: "appid", value: "b1b15e88f")
+            URLQueryItem(name: "title", value: "Moscow,DE"),
+            URLQueryItem(name: "userId", value: "1")
         ]
 
         //2 setting
         let config = URLSessionConfiguration.default
 
+        var request = URLRequest(url: urlCons.url!)
+        request.httpMethod = "POST"
 
         //3 session
         let session = URLSession(configuration: .default)
         // Do any additional setup after loading the view.
 
-        let task = session.dataTask(with: urlCons.url!) { data, res, err in
+        let task = session.dataTask(with: request) { data, res, err in
             if let err = err {
                 print(err.localizedDescription)
             } else {
