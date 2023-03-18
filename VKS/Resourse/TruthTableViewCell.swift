fe4232cc1483d59130dc1388e0fcc31c69b4f1c3
evgeniykolesnikov <mr.oneofus@gmail.com>
2023-03-18 21:24:38 +0300

    The third part of the homework. Make an animation of a change in the number of “I like”. It can be any animation: a coup from side to side, smooth change or leafing.

--- VKS/Resourse/TruthTableViewCell.swift
+++ VKS/Resourse/TruthTableViewCell.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class TruthTableViewCell: UITableViewCell {
-
+    
     // MARK: - IBOutlet
     
     @IBOutlet var avatarShadowImage: UIView!
     @IBOutlet var avatarImage: UIImageView!
     @IBOutlet var authorsNameTruthPost: UILabel!
-    @IBOutlet var dateAndTimePost: UILabel! 
+    @IBOutlet var dateAndTimePost: UILabel!
     @IBOutlet var textPost: UILabel!
     @IBOutlet var imagePost: UIImageView!
     @IBOutlet var likeParentView: UIView!
@@ -23,20 +23,22 @@ class TruthTableViewCell: UITableViewCell {
     @IBOutlet var container: UIView!
     @IBOutlet var containerLike: UIView!
     @IBOutlet var likeImageViewFront: UIImageView!
-
-
+    
+    @IBOutlet var likeCount: UILabel!
+    var count = 0
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // FIXME: CornerRadius displayed only when you click on Cell
-//        container.layer.cornerRadius = 2
+        //        container.layer.cornerRadius = 2
         container.layer.backgroundColor = UIColor.white.cgColor
-
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         tap.numberOfTapsRequired = 2
         container.addGestureRecognizer(tap)
-
-
+        
+        
         avatarImage.layer.cornerRadius = avatarImage.bounds.width / 2
         avatarShadowImage.layer.cornerRadius = avatarShadowImage.bounds.width / 2
         avatarShadowImage.layer.backgroundColor != UIColor.clear.cgColor
@@ -47,10 +49,10 @@ class TruthTableViewCell: UITableViewCell {
         avatarShadowImage.clipsToBounds = false
         containerLike.layer.backgroundColor = UIColor.gray.withAlphaComponent(0.5).cgColor
         containerLike.layer.cornerRadius = containerLike.bounds.width / 4
-
+        
         
     }
-
+    
     @objc func handleTap(_: UITapGestureRecognizer) {
         likeControl.isLike.toggle()
         if likeControl.isLike {
@@ -58,12 +60,21 @@ class TruthTableViewCell: UITableViewCell {
         } else {
             likeControl.likePicture.image = nil
         }
+        count += 1
+        print(count)
+            UIView.transition(with: likeCount, duration: 0.3, options: [.transitionCrossDissolve]) {
+                self.likeCount.text = ""
+                self.likeCount.text = "\(self.count)"
+            } completion: { _ in
+                
+            }
+        }
+        
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+            
+            // Configure the view for the selected state
+        }
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
